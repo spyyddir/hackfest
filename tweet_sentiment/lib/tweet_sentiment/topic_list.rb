0aaee5066b7c8712e590db5bfd6132523111067b
Zac Clark <hi@zacclark.com>
2011-08-31 22:01:03 -0600

    Add tests and popular features.

--- tweet_sentiment/lib/tweet_sentiment/topic_list.rb
+++ tweet_sentiment/lib/tweet_sentiment/topic_list.rb
@@ -8,5 +8,23 @@ module TweetSentiment
     end
     
     attr_reader :topics
+    
+    def most_popular
+      topics_sorted_by_sentiment.first
+    end
+    
+    def least_popular
+      topics_sorted_by_sentiment.reverse.first
+    end
+    
+    def most_controversial
+      @topics.sort_by {|t| (t.positive - t.negative).abs}.first
+    end
+    
+    private
+    
+    def topics_sorted_by_sentiment
+      @sorted ||= @topics.sort_by {|x| x.sentiment_index}
+    end
   end
 end
--- tweet_sentiment/test/mygem_test.rb
+++ tweet_sentiment/test/mygem_test.rb
@@ -4,11 +4,17 @@ module TweetSentiment
   class Topic
     private
     def fetch
+      # @@woo = 1
+      total = 100
+      pos = rand(50)
+      neg = rand(50)
+      neut = total - pos - neg
+      
       {
         "sentiment_index" => 0,
-        "positive" => 20,
-        "negative" => 10,
-        "neutral" => 70,
+        "positive" => pos,
+        "negative" => neg,
+        "neutral" => neut,
         "results" => [
           {"text" => "some example tweet", "sentiment" => 2}
         ]
@@ -26,6 +32,44 @@ class TweetSentimentTest < MiniTest::Unit::TestCase
   
   def test_that_we_can_extract_a_tweet
     assert_equal "some example tweet", Topic.new("whatever").tweets.first.text
+  end
+  
+  def test_topic_returns_a_list_of_tweets
+    topic = Topic.new("something")
+    assert_equal Array, topic.tweets.class
+    assert_equal Tweet, topic.tweets.first.class
+  end
+  
+  def test_tweet_has_text
+    tweet = Tweet.new("something", "data")
+    assert_equal "something", tweet.text
+  end
+  
+  def test_topiclist_returns_array_of_topics
+    list = TopicList.new "one", "two", "three"
+    
+    assert_equal Array, list.topics.class
+    list.topics.each do |topic|
+      assert_equal Topic, topic.class
+    end
+  end
+  
+  def test_topic_list_returns_most_popular_topic
+    list = TopicList.new "one", "two"
+    
+    assert_equal Topic, list.most_popular.class
+  end
+  
+  def test_topic_list_returns_least_popular_topic
+    list = TopicList.new "one", "two"
+    
+    assert_equal Topic, list.least_popular.class
+  end
+  
+  def test_topic_list_returns_most_controversial
+    list = TopicList.new "one", "two"
+    
+    assert_equal Topic, list.most_controversial.class
   end
   
 end
